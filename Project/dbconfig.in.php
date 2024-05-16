da3284d50e03c79b1191622901ae6e80e7a4bb9b
Mohammed Taha <93288009+mohaTaha23@users.noreply.github.com>
2024-05-16 19:58:17 +0300

    Update dbconfig.in.php

--- Project/dbconfig.in.php
+++ Project/dbconfig.in.php
@@ -2,6 +2,6 @@
 define('DBHOST', 'localhost');
 define('DBNAME', 'web_project');
 define('DBUSER', 'root');
-define('DBPASS', 'tahasql4873');
+define('DBPASS', 'yourpassword');
 define('DBCONNSTRING',"mysql:host=". DBHOST. ";dbname=". DBNAME);
 ?>
